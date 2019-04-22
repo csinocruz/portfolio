54517fe1f96bebe4309df5cd930ec8789b295421
Cesca <csinocruz@gmail.com>
2019-04-22 13:51:56 -0700

    functions to add colors to arrays are created and working

--- main.js
+++ main.js
@@ -1,9 +1,54 @@
 $(document).ready(function(){
-        console.log('hello');
         var color = $('.box')
         $(color).click(function(){
-            var value = $(this).text();
-            console.log(value);
+            var value = $(this).text().trim();
+            addColorToArray(value);
         })
     }
 );
+
+var array = [];
+var playerArray = [];
+
+function pickRandomColor(){
+    var randomColor = Math.floor(Math.random() * 4)+1;
+
+    switch(randomColor){
+        case 1: 
+        randomColor = 'green box';
+        break;
+        case 2: 
+        randomColor = 'pink box';
+        break;
+        case 3: 
+        randomColor = 'yellow box';
+        break;
+        case 4: 
+        randomColor = 'orange box';
+        break;
+        default:
+        console.log('something broke');
+    }
+    array.push(randomColor);
+}
+
+// adds a color to the player array
+function addColorToArray(colorValue){
+    playerArray.push(colorValue);
+}
+
+function pressPlay(){
+    for(var i=0; i<array.length; i++){
+        console.log(array[i]);
+    }
+}
+
+function checkArrays(){
+    for(var i=0; i<array.length; i++){
+        if(array[i] === playerArray[i]){
+            console.log('CORRECT!')
+        } else {
+            console.log('WRONG!');
+        }
+    }
+}
